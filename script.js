f6bd2f96f1addd70520bba5831394fe1ecebf912
Mika <michael.aubras@gmail.com>
2022-11-07 15:13:46 +0100

    modification header & sidebar

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-document.getElementById("hamburger").addEventListener("click", function () {
-    document.getElementById("mySidebar").style.width = "300px";
-    document.getElementById("mySidebar").style.transition = "0.6s";
-    document.getElementById("layout").style.marginLeft = "300px";
-    document.getElementById("layout").style.transition = "0.6s";
-
-})
-
-document.getElementById("closebtn").addEventListener("click", function () {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("mySidebar").style.transition = "0.6s";
-    document.getElementById("layout").style.marginLeft = "0";
-    document.getElementById("layout").style.transition = "0.6s";
-})
+/*********************************************************/
+/************************ SIDEBAR ***********************/
+/*******************************************************/
+var btn = document.querySelector('.toggle');
+var btnst = true;
+btn.onclick = function () {
+    if (btnst == true) {
+        document.querySelector('.toggle span').classList.add('toggle');
+        document.getElementById('mySidebar').classList.add('sidebarshow');
+        document.getElementById("layout").style.marginLeft = "340px";
+        document.getElementById("layout").style.transition = "0.6s";
+        btnst = false;
+    } else if (btnst == false) {
+        document.querySelector('.toggle span').classList.remove('toggle');
+        document.getElementById("layout").style.marginLeft = "0";
+        document.getElementById('mySidebar').classList.remove('sidebarshow');
+        document.getElementById("layout").style.transition = "0.6s";
+        btnst = true;
+    }
+}
